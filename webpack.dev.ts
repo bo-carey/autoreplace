3f7b5d624cbe9a4336fd548a75420aa7027937bd
jelly <bo.carey@outlook.com>
2021-10-03 20:59:41 -0400

    Added webext plugin

--- webpack.dev.ts
+++ webpack.dev.ts
@@ -1,10 +1,25 @@
+import * as path from 'path';
 import { Configuration } from 'webpack';
 import merge from 'webpack-merge';
+import WebExtPlugin from 'web-ext-plugin';
 import commonConfig from './webpack.common';
+
+const buildTarget: string = process.env.BUILD_TARGET || 'firefox';
+const gameLocation = process.env.GAME_LOCATIONS || '';
 
 const config: Configuration = merge(commonConfig, {
   devtool: 'source-map',
   mode: 'development',
+  plugins: [
+    new WebExtPlugin({
+      sourceDir: path.resolve(__dirname, `dist/${buildTarget}`),
+      artifactsDir: path.resolve(__dirname, `dist/${buildTarget}`),
+      buildPackage: true,
+      overwriteDest: true,
+      browserConsole: true,
+      startUrl: gameLocation,
+    }),
+  ],
 });
 
 export default config;
--- webpack.prod.ts
+++ webpack.prod.ts
@@ -1,12 +1,25 @@
+import * as path from 'path';
 import { Configuration } from 'webpack';
 import merge from 'webpack-merge';
+import WebExtPlugin from 'web-ext-plugin';
 import commonConfig from './webpack.common';
+
+const buildTarget: string = process.env.BUILD_TARGET || 'firefox';
 
 const config: Configuration = merge(commonConfig, {
   optimization: {
     minimize: true,
   },
   mode: 'production',
+  plugins: [
+    new WebExtPlugin({
+      sourceDir: path.resolve(__dirname, `dist/${buildTarget}`),
+      artifactsDir: path.resolve(__dirname, `dist/${buildTarget}`),
+      buildPackage: true,
+      overwriteDest: true,
+      outputFilename: `${buildTarget}.zip`,
+    }),
+  ],
 });
 
 export default config;
