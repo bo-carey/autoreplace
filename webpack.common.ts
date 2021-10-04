9cc01844553c42f763ef35be626fe8ebff2a1d20
Bo Carey <Bo.Carey@outlook.com>
2021-10-04 14:51:05 -0400

    Moved WebExtPlugin to common webpack config

--- webpack.common.ts
+++ webpack.common.ts
@@ -1,8 +1,10 @@
 import * as path from 'path';
 import { Configuration } from 'webpack';
 import CopyWebpackPlugin from 'copy-webpack-plugin';
+import WebExtPlugin from 'web-ext-plugin';
 
 const buildTarget: string = process.env.BUILD_TARGET || 'firefox';
+const gameLocation = process.env.GAME_LOCATION || '';
 
 const config: Configuration = {
   entry: {
@@ -43,6 +45,16 @@ const config: Configuration = {
   plugins: [
     new CopyWebpackPlugin({
       patterns: [{ from: 'target/shared/' }, { from: `target/${buildTarget}/` }],
+    }),
+    new WebExtPlugin({
+      sourceDir: path.resolve(__dirname, `dist/${buildTarget}`),
+      artifactsDir: path.resolve(__dirname, `dist`),
+      buildPackage: true,
+      overwriteDest: true,
+      outputFilename: `${buildTarget}.zip`,
+      browserConsole: false,
+      startUrl: gameLocation,
+      firefoxProfile: 'default',
     }),
   ],
   stats: {
