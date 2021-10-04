1aac8559be6cc82d11b1fc951d842699166cde92
Bo Carey <Bo.Carey@outlook.com>
2021-10-04 14:59:37 -0400

    Popup component to notify that it has mounted

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-import { runBackstabbrAssistant } from './scripts/backstabbr';
+import { browser } from 'webextension-polyfill-ts';
+// import { runBackstabbrAssistant } from './scripts/backstabbr';
 console.dir('starting backstabbr assistant');
-runBackstabbrAssistant();
+// runBackstabbrAssistant();
+
+browser.runtime.onMessage.addListener((request: { popupMounted: boolean }) => {
+  if (request.popupMounted) {
+    console.log('backgroundPage notified that Popup.tsx has mounted.');
+  }
+});
--- src/popup/component.tsx
+++ src/popup/component.tsx
@@ -1,5 +1,10 @@
 import React, { FunctionComponent } from 'react';
+import { browser } from 'webextension-polyfill-ts';
 
 export const Popup: FunctionComponent = () => {
+  React.useEffect(() => {
+    browser.runtime.sendMessage({ popupMounted: true });
+  }, []);
+
   return <div></div>;
 };
