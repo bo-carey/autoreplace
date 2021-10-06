1baed1e64b522b9e096fadd9c230a76e27e99d17
Bo Carey <Bo.Carey@outlook.com>
2021-10-05 21:36:28 -0400

    Added new find and replace UI

--- src/popup/component.tsx
+++ src/popup/component.tsx
@@ -6,5 +6,24 @@ export const Popup: FunctionComponent = () => {
     browser.runtime.sendMessage({ popupMounted: true });
   }, []);
 
-  return <div></div>;
+  return (
+    <div id="cont">
+      <button>&darr;</button>
+      <div id="rows">
+        <div className="row">
+          <input type="text" />
+          <span>? of 2</span>
+          <button>&uarr;</button>
+          <button>&darr;</button>
+          <button>&#9776</button>
+          <button>&times;</button>
+        </div>
+        <div className="row">
+          <input type="text" />
+          <button>&#9735;</button>
+          <button>&#9736;</button>
+        </div>
+      </div>
+    </div>
+  );
 };
