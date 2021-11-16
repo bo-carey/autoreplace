a5a38bad061db1d71e510d71d6d163dc6fd2410f
jelly <bo.carey@outlook.com>
2021-11-15 19:11:16 -0500

    Simplified siteSettings fetching process

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,36 +1,46 @@
 import { browser } from 'webextension-polyfill-ts';
 import { matchUrl } from '@bo-carey/urlglob';
 
-import { SiteKey, SiteSettings } from './constants';
+import { SiteSettings } from './constants';
 
-const getAllKeys = async (): Promise<SiteKey[]> => {
-  const data = await browser.storage.sync.get('allKeys');
-  const allKeys = data?.allKeys || [];
-  return allKeys;
+// const getAllKeys = async (): Promise<SiteKey[]> => {
+//   const data = await browser.storage.sync.get('allKeys');
+//   const allKeys = data?.allKeys || [];
+//   return allKeys;
+// };
+
+// const getSiteKey = async (location = window.location.href): Promise<SiteKey | null> => {
+//   const allKeys = await getAllKeys();
+//   const siteKey = allKeys.find((k) => matchUrl(location, k.urlGlob));
+//   return siteKey || null;
+// };
+
+// const setAllKeys = async (allKeys: SiteKey[]): Promise<void> => {
+//   await browser.storage.sync.set({ allKeys });
+// };
+
+// const setSiteKey = async (siteKey: SiteKey): Promise<void> => {
+//   const allKeys = await getAllKeys();
+//   await setAllKeys(allKeys.map((k) => (siteKey.uuid === k.uuid ? siteKey : k)));
+// };
+
+const getAllSiteSettings = async (): Promise<{ [s: string]: any }> => {
+  const data = await browser.storage.sync.get();
+  return data;
 };
 
-const getSiteKey = async (location = window.location.href): Promise<SiteKey | null> => {
-  const allKeys = await getAllKeys();
-  const siteKey = allKeys.find((k) => matchUrl(location, k.urlGlob));
-  return siteKey || null;
-};
-
-const setAllKeys = async (allKeys: SiteKey[]): Promise<void> => {
-  await browser.storage.sync.set({ allKeys });
-};
-
-const setSiteKey = async (siteKey: SiteKey): Promise<void> => {
-  const allKeys = await getAllKeys();
-  await setAllKeys(allKeys.map((k) => (siteKey.uuid === k.uuid ? siteKey : k)));
-};
-
-const getSiteSettings = async (): Promise<SiteSettings | null> => {
-  const siteKey = await getSiteKey();
-  const uuid = siteKey?.uuid;
-  if (!uuid || !uuid?.length) return null;
-  const { [uuid]: siteSettings } = await browser.storage.sync.get(uuid);
-  if (siteSettings.uuid !== uuid) return null;
-  return siteSettings;
+const getSiteSettings = async (
+  location = window.location.href,
+): Promise<SiteSettings | null> => {
+  const allSiteSettings = await getAllSiteSettings();
+  for (const siteSettings of Object.values(allSiteSettings)) {
+    if (matchUrl(location, siteSettings.urlGlob)) {
+      console.log(`siteSettings`, siteSettings);
+      return siteSettings;
+    }
+  }
+  console.dir('no site settings found');
+  return null;
 };
 
 const setSiteSettings = async (siteSettings: SiteSettings): Promise<void> => {
@@ -39,10 +49,10 @@ const setSiteSettings = async (siteSettings: SiteSettings): Promise<void> => {
 };
 
 export default {
-  getAllKeys,
-  getSiteKey,
-  setAllKeys,
-  setSiteKey,
+  // getAllKeys,
+  // getSiteKey,
+  // setAllKeys,
+  // setSiteKey,
   getSiteSettings,
   setSiteSettings,
 };
